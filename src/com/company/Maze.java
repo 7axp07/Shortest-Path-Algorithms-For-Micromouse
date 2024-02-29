9f53f862369d6507f8a89c8012b64c3728dcf4e4
a <128128550+furretcat@users.noreply.github.com>
2024-02-29 22:06:21 +0100

    WORKs on smaller mazes too now just generate them n all that

---
+++ src/com/company/Maze.java
@@ -0,0 +1,67 @@
+package com.company;
+
+public class Maze {
+
+    //10 x 10
+    public static final int[][] MAZE_10_1 = {
+            { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 },
+            { 1, 0, 0, 1, 0, 1, 0, 1, 0, 1 },
+            { 1, 1, 0, 0, 0, 0, 0, 1, 0, 1 },
+            { 1, 1, 1, 1, 0, 1, 1, 1, 0, 1 },
+            { 1, 0, 0, 1, 0, 0, 0, 0, 0, 1 },
+            { 1, 1, 0, 1, 1, 1, 0, 1, 1, 1 },
+            { 1, 1, 0, 1, 0, 0, 0, 0, 0, 1 },
+            { 1, 1, 0, 1, 1, 1, 1, 1, 0, 1 },
+            { 1, 2, 0, 0, 0, 0, 0, 0, 0, 1 },
+            { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 }
+    };
+
+    public static final int[][] MAZE_10_2 = {
+
+
+    };
+
+    public static final int[][] MAZE_10_3 = {
+
+
+    };
+
+    public static final int[][] MAZE_10_4 = {
+
+
+    };
+
+    public static final int[][] MAZE_10_5 = {
+
+
+    };
+
+    public static final int[][] MAZE_10_6 = {
+
+
+    };
+
+    public static final int[][] MAZE_10_7 = {
+
+
+    };
+
+    public static final int[][] MAZE_10_8 = {
+
+
+    };
+
+    public static final int[][] MAZE_10_9 = {
+
+
+    };
+
+    public static final int[][] MAZE_10_10 = {
+
+
+    };
+
+
+    // 20 x 20
+
+}
